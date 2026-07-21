9e792253c240b2bcab31dab650821f57c8e49dea
AilurusRP <AilurusRP@hotmail.com>
2026-07-21 12:26:46 +0800

    feat: add message receiving from web and mobile clients

--- lib/src/start_server.dart
+++ lib/src/start_server.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:lanshare_cli/dep_wrappers/sse_wrapper.dart';
@@ -8,12 +9,38 @@ import 'package:shelf_static/shelf_static.dart';
 
 final sseClients = <SsePublisher>[];
 
+class MsgResponse {
+  MsgResponse(this.code);
+  int code;
+
+  Map<String, int> toJson() => {"code": code};
+}
+
 startServer() async {
   final app = Router();
+
   app.get("/events", (Request r) {
     final publisher = SsePublisher();
     sseClients.add(publisher);
     return getSseHandler(publisher)(r);
+  });
+
+  app.post("/web-msg", (Request r) async {
+    final body = await r.readAsString();
+    final msg = jsonDecode(body)["msg"];
+    print("-------------MESSAGE FROM LANShare-web-------------");
+    print(msg);
+    print("----------------------------------------------------");
+    return Response.ok(jsonEncode(MsgResponse(200)));
+  });
+
+  app.post("/mobile-msg", (Request r) async {
+    final body = await r.readAsString();
+    final msg = jsonDecode(body)["msg"];
+    print("-------------MESSAGE FROM LANShare-mobile-------------");
+    print(msg);
+    print("----------------------------------------------------");
+    return Response.ok(jsonEncode(MsgResponse(200)));
   });
 
   var webHandler =
